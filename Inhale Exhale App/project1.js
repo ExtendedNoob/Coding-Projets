447af89be011fc96137295e001e79324c0b30f92
ExtendedNoob <93956198+ExtendedNoob@users.noreply.github.com>
2021-11-08 20:27:31 -0500

    Create project1.js

---
+++ Inhale Exhale App/project1.js
@@ -0,0 +1,24 @@
+const contenant = document.getElementById('contenant');
+const texte = document.getElementById('texte');
+
+const tempsTotal = 9000;
+const tempsInspirer = (tempsTotal / 5) * 2;
+const tempsPause = tempsTotal / 5;
+
+AnimationRespire();
+
+function AnimationRespire() {
+	texte.innerText = 'Inspire! 😮';
+	contenant.className = 'contenant grandir';
+
+	setTimeout (() => {
+		texte.innerText = 'Pause... 😶';
+
+		setTimeout(() => {
+			texte.innerText = 'Expire! 😮‍💨';
+			contenant.className = 'contenant rapetisser';
+		}, tempsPause);
+	}, tempsInspirer);
+}
+
+setInterval(AnimationRespire, tempsTotal);
